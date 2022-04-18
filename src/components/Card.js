81070240419ccbdcf3a6033c92f42a1cba5949f3
sf-pchakraborty <chakraborty.priyanka@gmail.com>
2022-04-18 12:13:16 +0530

    warnings

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,11 @@ export default function Card(props) {
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`../images/${props.coverImg}`} className="card--image" />
+      <img
+        src={`../images/${props.coverImg}`}
+        alt="cover image"
+        className="card--image"
+      />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" />
         <span>{props.stats.rating}</span>
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default [
+const data = [
   {
     id: 1,
     title: 'Life Lessons with Katie Zaferes',
@@ -42,3 +42,5 @@ export default [
     openSpots: 3,
   },
 ]
+
+export default data
