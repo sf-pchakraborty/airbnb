5649eb91411a9e59f47a1ff8d7f57d732308a2d0
sf-pchakraborty <chakraborty.priyanka@gmail.com>
2022-04-18 12:26:37 +0530

    warnings

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,7 +12,7 @@ export default function Card(props) {
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={`../images/${props.coverImg}`} alt="" className="card--image" />
       <div className="card--stats">
-        <img src="../images/star.png" className="card--star" />
+        <img src="../images/star.png" alt="" className="card--star" />
         <span>{props.stats.rating}</span>
         <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
